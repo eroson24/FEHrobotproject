dd689ea010221860852b7027f5f0212f61017190
Erol Sonmez <erolsonmeznj@gmail.com>
2025-02-07 10:10:20 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/********************************/
+/*      Proteus Test Code       */
+/*     OSU FEH Spring 2020      */
+/*        Drew Phillips         */
+/*    02/03/20  Version 3.0.1     */
+/********************************/
+
+// AM 02/03/20
+
+/* Include preprocessor directives */
+#include <FEHLCD.h>
+#include <FEHIO.h>
+#include <FEHUtility.h>
+#include <FEHMotor.h>
+#include <FEHServo.h>
+#include <FEHAccel.h>
+#include <FEHBattery.h>
+#include <FEHBuzzer.h>
+#include <FEHRCS.h>
+#include <FEHSD.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(void)
+{
+    LCD.Clear(BLACK);
+    LCD.WriteLine("Hello, World!");
+	return 0;
+}
