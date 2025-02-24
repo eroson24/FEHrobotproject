9de8a639e2bda6a81315d5033b4547291db9f2df
Mantis-Shrimp9 <oyster.34@buckeyemail.osu.edu>
2025-02-24 09:57:28 -0500

    moveRobot, detectLine, turn

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /****************************************/
-/*      Proteus Test Code R04_1         */
+/*      Proteus Exploration #2          */
 /*      OSU FEH Spring 2025             */
 /*      Mark Oyster, Erol Sonmez,       */
 /*      Reagan Massey, Austin Toczynski */
-/*      02/03/20  Version 3.0.1         */
+/*      02/14/2025  Version 3.0.1       */
 /****************************************/
 
 /* Include preprocessor directives */
@@ -25,6 +25,51 @@ FEHMotor right_motor(FEHMotor::Motor0,9.0);
 FEHMotor left_motor(FEHMotor::Motor0,9.0);
 FEHMotor back_motor(FEHMotor::Motor0,9.0);
 FEHMotor tread_motor(FEHMotor::Motor0,9.0);
+
+int main(void)
+{
+    
+    
+    return 0;
+}
+
+
+void moveRobot(int strength, double time, int direction) {
+  //Reset encoder counts
+
+  /*
+  For direction:
+  1 is forward
+  2 is back right
+  3 is back left
+  */
+
+  //move forward for time
+  if (direction == 1){
+    right_motor.SetPercent(strength);
+    left_motor.SetPercent(strength);
+    Sleep(time);
+  }
+
+  //move back right for time
+  if (direction == 2){
+    right_motor.SetPercent(strength);
+    back_motor.SetPercent(strength);
+    Sleep(time);
+  }
+
+  //move back left for time
+  if (direction == 3){
+    left_motor.SetPercent(strength);
+    back_motor.SetPercent(strength);
+    Sleep(time);
+  }
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
+
+}
 
 // declare optosensor
 AnalogInputPin optosensor(FEHIO::P1_0);
@@ -62,9 +107,5 @@ int turn(int strengthPercent, double seconds, bool clockwise) {
     return 0;
 }
 
-int main(void)
-{
+  
 
-    
-    return 0;
-}
