b810360826d65ec29021540c96dcf0afba410705
eroson24 <sonmez.12@osu.edu>
2025-03-25 19:02:53 -0400

    Milestone 3 Distance sensor navigation with shaft encoding

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 /****************************************/
 
 /* Include preprocessor directives */
-
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
@@ -18,210 +17,86 @@
 #include <FEHBuzzer.h>
 #include <FEHRCS.h>
 #include <FEHSD.h>
-#include <stdio.h>
 #include <string.h>
-
-//declare motors.
-FEHMotor tread_motor(FEHMotor::Motor0,9.0);
-FEHMotor right_motor(FEHMotor::Motor1,9.0);
-FEHMotor left_motor(FEHMotor::Motor2,9.0);
-FEHMotor back_motor(FEHMotor::Motor3,9.0);
+#include <stdio.h>
 
 
-void moveRobotTime(int strength, double time, char direction[]) {
+//Declarations for encoders & motors
+DigitalEncoder right_encoder(FEHIO::P0_0);
+DigitalEncoder left_encoder(FEHIO::P0_1);
+FEHMotor right_motor(FEHMotor::Motor2,9.0);
+FEHMotor left_motor(FEHMotor::Motor1,9.0);
+DigitalInputPin distanceSensor(FEHIO::P3_0);
 
-  //move forward for time
-  if (strcmp(direction, "forward") == 0){
-    right_motor.SetPercent(-strength);
-    left_motor.SetPercent(strength);
-    Sleep(time);
-  }
-  //move back right for time
-  else if (strcmp(direction, "backRight") == 0){
-    right_motor.SetPercent(strength);
-    back_motor.SetPercent(-strength);
-    Sleep(time);
-  }
-  //move back left for time
-  else if (strcmp(direction, "backLeft") == 0){
-    left_motor.SetPercent(-strength);
-    back_motor.SetPercent(strength);
-    Sleep(time);
-  }
-  else if (strcmp(direction, "backward") == 0){
-    right_motor.SetPercent(strength);
-    left_motor.SetPercent(-strength);
-    Sleep(time);
-  }
+void move_forward(int percent, int counts) //using encoders
+{
+    //Reset encoder counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+    //Set both motors to desired percent
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(percent);
+
+    //While the average of the left and right encoder is less than counts,
+    //keep running motors
+    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
     //Turn off motors
     right_motor.Stop();
     left_motor.Stop();
-    back_motor.Stop();
-
 }
 
-void moveRobot(int strength, char direction[]) {
+void turn_right(int percent, int counts, bool rightTurn) //using encoders
+{
+ //Reset encoder counts
+ right_encoder.ResetCounts();
+ left_encoder.ResetCounts();
+ //Set both motors to desired percent
+ if (rightTurn) {
+ right_motor.SetPercent(-25);
+ left_motor.SetPercent(25);
+} else {
+    right_motor.SetPercent(25);
+    left_motor.SetPercent(-25); 
+}
+ //While the average of the left and right encoder is less than counts,
+ //keep running motors
+ while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
 
-  //move forward for time
-  if (strcmp(direction, "forward") == 0){
-    right_motor.SetPercent(-strength);
-    left_motor.SetPercent(strength);
-  }
-  //move back right for time
-  else if (strcmp(direction, "backRight") == 0){
-    right_motor.SetPercent(strength);
-    back_motor.SetPercent(-strength);
-  }
-  //move back left for time
-  else if (strcmp(direction, "backLeft") == 0){
-    left_motor.SetPercent(-strength);
-    back_motor.SetPercent(-strength);
-  }
-  else if (strcmp(direction, "backward") == 0){
-    left_motor.SetPercent(-strength);
-    right_motor.SetPercent(strength);
-  }
+ //Turn off motors
+ right_motor.Stop();
+ left_motor.Stop();
 }
 
-// declare optosensor
-AnalogInputPin optosensor(FEHIO::P1_0);
+int main(void)
+{
 
-// detectLine function for robot
-bool detectLine(int voltage) {
-    bool voltageDetected;
-    if (optosensor.Value() > voltage) {
-        voltageDetected = true;
-    } else {
-        voltageDetected = false;
-    }
-    return voltageDetected;
-}
+    float x, y; //for touch screen
 
-//clockwise is true
-// turn function for robot
-int turn(int strengthPercent, double seconds, bool clockwise) {
-    if (clockwise) {
-        right_motor.SetPercent(strengthPercent);
-        left_motor.SetPercent(strengthPercent);
-        back_motor.SetPercent(strengthPercent);
-        Sleep(seconds);
-        right_motor.SetPercent(0);
-        left_motor.SetPercent(0);
-        back_motor.SetPercent(0);
-    } else {
-        right_motor.SetPercent(-strengthPercent);
-        left_motor.SetPercent(-strengthPercent);
-        back_motor.SetPercent(-strengthPercent);
-        Sleep(seconds);
-        right_motor.SetPercent(0);
-        left_motor.SetPercent(0);
-        back_motor.SetPercent(0);
-    }
-    return 0;
-}
-
-//function for finding actual power from proteus battery level
-float actualPower(float desiredPower) {
-  float batteryVoltage = Battery.Voltage();
-  if (batteryVoltage > 11.5){
-    batteryVoltage == 11.5;
-  }
-  float actualPower = ((11.5/batteryVoltage) * desiredPower);
-  return actualPower;
-  }
-  
-  //Declare CdS cell sensor
-  
-  
-  int main(void)
-  {
-     AnalogInputPin CdS_cell(FEHIO::P0_0);
-
-    // declare light sensor input pin
-    const float RED_VALUE = .1;
-    const float BLUE_VALUE = .7;
-    
-    //Clear Screen for writing
-   
+    //Initialize the screen
+    LCD.Clear(BLACK);
     LCD.SetFontColor(WHITE);
-    LCD.Clear();
-    Sleep(.1);
+
+    LCD.WriteLine("Touch the screen");
+    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
+    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+
+    move_forward(25, 450);
     
-   
-    // declare constants
-    float CdS_Value = 10;
-    //for 25 power
-    const float NINETY_DEGREE_TURN = 0.49;
-    const float FOURTYFIVE_DEGREE_TURN = 0.5 * NINETY_DEGREE_TURN;
-    char Forward[] = "forward";
-    char Backward[] = "backward";
-    char backRight[] = "backRight";
-    char backLeft[] = "backLeft";
-    float x, y;
-    int buttonSide = 0;
-    Sleep(.1);
-    float strength = 70;
-    double time = 0.5;
-
-  //wait until red light turns on to start
-  while (CdS_Value > RED_VALUE)
-  {
-    CdS_Value = CdS_cell.Value();
-  }
-
-    //Turn from start area
-    turn(strength, NINETY_DEGREE_TURN, true);
-    Sleep(1.0);
-    
-    //move to center of ramp
-    time = .26;
-    moveRobotTime(strength, time, Forward);
-    Sleep(1.0);
-
-    //turn towards ramp
-    turn(strength, FOURTYFIVE_DEGREE_TURN, false);
-    Sleep(1.0);
-    
-  
-    //move up ramp
-    time = 1.725;
-    strength = 100;
-    moveRobotTime(strength, time, Forward);
-    Sleep(1.0);
-  
-    //turn so back is facing window
-    time = NINETY_DEGREE_TURN;
-    strength = 70;
-    turn(strength, time, true);
-    Sleep(1.0);
-
-    //go backwards
-    time = 1.1;
-    strength = 100;
-    moveRobotTime(strength, time, Backward);
-    Sleep(1.0);
-
-    // WINDOW SHOULD BE CLOSED NOW
-    
-    time = 0.26;
-    strength = 70;
-    moveRobotTime(strength, time, backLeft);
-    moveRobotTime(strength, time, backRight);
-    Sleep(1.0);
-    
-
-    //go forward to close window
-    time = 0.8;
-    strength = 100;
-    moveRobotTime(strength, time, Forward);
-
-/*
-  RCS.InitializeTouchMenu("0910B8VYV");
-  int lever = RCS.GetLever();
-  LCD.WriteLine(RCS.Time());
-  */
-
-  return 0;
-    
+    if (distanceSensor.Value() != 0) {
+        // if wall isnt detected
+        while (distanceSensor.Value() != 0) 
+        {
+            // keep moving backwards until it is
+            move_forward(25, 1);
+        }
+    } else { // else if wall is detected
+        while (distanceSensor.Value() == 0) 
+        {
+            // moves forwards until it isnt
+            move_forward(-25, 1);
+        }
+        
+    }
 }
-
